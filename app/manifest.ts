ebe24f957e245a05f1e1592c7337d27c746b5c9e
M.S.Praveen <m.s.praveen00@gmail.com>
2026-03-06 22:38:11 +0530

    Logo installation fixed

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,25 +11,25 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#000000',
         icons: [
             {
-                src: '/icon.jpg',
+                src: '/icon.png',
                 sizes: '192x192',
-                type: 'image/jpeg',
+                type: 'image/png',
             },
             {
-                src: '/icon.jpg',
+                src: '/icon.png',
                 sizes: '192x192',
-                type: 'image/jpeg',
+                type: 'image/png',
                 purpose: 'maskable',
             },
             {
-                src: '/icon.jpg',
+                src: '/icon.png',
                 sizes: '512x512',
-                type: 'image/jpeg',
+                type: 'image/png',
             },
             {
-                src: '/icon.jpg',
+                src: '/icon.png',
                 sizes: '512x512',
-                type: 'image/jpeg',
+                type: 'image/png',
                 purpose: 'maskable',
             },
         ],
